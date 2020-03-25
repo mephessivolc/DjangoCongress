8c637cce578fb14df7a9639a2fa918c3a574a726
clovis caface <clovis.caface@gmail.com>
2020-03-25 20:49:11 -0300

    Rename Project

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoevent.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocongress.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
