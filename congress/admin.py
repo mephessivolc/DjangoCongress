e32c24f44d3ccd9ee47b00d7cb0386c834c8dc32
clovis caface <clovis.caface@gmail.com>
2020-04-04 11:55:28 -0300

    Create congress app

--- congress/admin.py
+++ congress/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TypeCongress, Congress, Subscriptions
 
 # Register your models here.
+admin.site.register(TypeCongress)
+admin.site.register(Congress)
+admin.site.register(Subscriptions)
--- congress/models.py
+++ congress/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+Users = get_user_model()
+
+class TypeCongress(models.Model):
+    """
+        Armazena tipo de congressos, exemplo: Colóquio, Simpósios, Semana de Estudos, entre outros
+    """
+    type_congress = models.CharField('Tipo de Congresso', max_length=50)
+
+    def __str__(self):
+        return self.type_congress
+
+class Congress(models.Model):
+    """
+        Dados essenciais dos Congressos
+    """
+
+    username = models.CharField('Apelido do evento', max_length=20)
+    name = models.CharField('Nome do Evento', max_length=100)
+    date_start_subscription = models.DateTimeField("Data/Hora para iniciar inscrições", auto_now=True)
+    date_start_congress = models.DateTimeField("Data/Hora para iniciar primeiro dia", auto_now=True)
+    date_close_subscription = models.DateTimeField("Data/Hora para finalizar o recebimento das inscrições", auto_now=True)
+    date_close_congress = models.DateTimeField("Data/hora da finalização do evento", auto_now=True)
+    date_close_awards = models.DateTimeField("Data/hora finalizar inscrições com premiação", auto_now=True)
+
+    class Meta:
+        verbose_name = 'Congresso'
+        verbose_name_plural = 'Congressos'
+
+    def __str__(self):
+        return self.name
+
+class Subscriptions(models.Model):
+
+    """
+        Inscritos
+    """
+    congress = models.ForeignKey(Congress, verbose_name='Congresso')
+    user = models.ForeignKey(Users, verbose_name='Usuario inscrito')
+
+    class Meta:
+        verbose_name = 'Inscrição'
+        verbose_name_plural = 'Inscrições'
+
+    def __str__(self):
+        return "{} ({})".format(self.user.name, self.congress.name)
