be0c835b8254eec4506c3d2d84f5508dc51a884d
clovis caface <clovis.caface@gmail.com>
2020-04-08 09:03:12 -0300

    Refactoring python decouple in DEBUG and ALLOWED_HOSTS. Close #6

--- djangocongress/settings.py
+++ djangocongress/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
