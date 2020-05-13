a5f8b248b51e7fa283535c75720d5e4702137cc9
clovis caface <clovis.caface@gmail.com>
2020-05-13 10:18:06 -0300

    Create Models: Institute, Curses

--- institute/models.py
+++ institute/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Institute(models.Model):
+    """
+        Institutos que tem ou terá relacao com o Congresso, seja para sediar ou para compor instituicoes onde
+        inscritos pertencem.
+    """
+    username = models.CharField('Sigla da Unidade de Ensino', max_length=15)
+    name = models.CharField('Nome da Unidade de Ensino', max_length=100)
+
+    class Meta:
+        verbose_name = 'Instituição de Ensino'
+        verbose_name_plural = 'Instituições de Ensino'
+
+    def __str__(self):
+        return self.name
+
+class Courses(models.Model):
+    """
+        Cursos que tem relacao com o congresso, seja para atualizar como compor curso onde
+        inscritos pertencem.
+    """
+    name = models.CharField('Curso', max_length=30)
+    modal = models.CharField('Modalidade', max_length=10, choices=modal_choices, default='L')
+    institute = models.ForeignKey(Institute, verbose_name='Unidade de Ensino', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
+    def get_readable_choices(self):
+        return dict(modal_choices)[self.modal]
+
+    def __str__(self):
+        return "{} - {} ({})".format(self.name, self.modal, self.institute.username)
