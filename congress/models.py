44bac645315ee65996d9004cbf514df0c5a6ff8f
clovis caface <clovis.caface@gmail.com>
2020-05-13 10:11:44 -0300

    Modifyingd date_start_congress and date_close_congress to date_start and data_close. Modifying returns in date. Separated columns in image models to other models: logoimages, instituteimages, certificateimages

--- congress/models.py
+++ congress/models.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.html import format_html
@@ -39,6 +40,10 @@ class TypeCongress(models.Model):
         return self.type_congress
 
 class Institute(models.Model):
+    """
+        Institutos que tem ou terá relacao com o Congresso, seja para sediar ou para compor instituicoes onde
+        inscritos pertencem.
+    """
     username = models.CharField('Sigla da Unidade de Ensino', max_length=15)
     name = models.CharField('Nome da Unidade de Ensino', max_length=100)
 
@@ -50,6 +55,10 @@ class Institute(models.Model):
         return self.name
 
 class Courses(models.Model):
+    """
+        Cursos que tem relacao com o congresso, seja para atualizar como compor curso onde
+        inscritos pertencem.
+    """
     name = models.CharField('Curso', max_length=30)
     modal = models.CharField('Modalidade', max_length=10, choices=modal_choices, default='L')
     institute = models.ForeignKey(Institute, verbose_name='Unidade de Ensino', on_delete=models.CASCADE)
@@ -68,15 +77,14 @@ class Congress(models.Model):
     """
         Dados essenciais dos Congressos
     """
-
     username = models.CharField('Apelido do evento', max_length=20, blank=True, null=True)
     slug = models.SlugField('Referencia', max_length=100, blank=True, null=True, default="")
     name = models.CharField('Nome do Evento', max_length=100)
     type_congress = models.ForeignKey(TypeCongress, verbose_name='Tipo do evento', on_delete=models.CASCADE)
     date_start_subscription = models.DateTimeField("Iniciar Inscrições", auto_now=False, auto_now_add=False, default=timezone.now)
-    date_start_congress = models.DateTimeField("Primeiro dia", auto_now=False, auto_now_add=False, default=timezone.now)
+    date_start = models.DateTimeField("Primeiro dia", auto_now=False, auto_now_add=False, default=timezone.now)
     date_close_subscription = models.DateTimeField("Encerrar Recebimento de Inscrições", auto_now=False, auto_now_add=False, default=timezone.now)
-    date_close_congress = models.DateTimeField("Finalização do evento", auto_now=False, auto_now_add=False, default=timezone.now)
+    date_close = models.DateTimeField("Finalização do evento", auto_now=False, auto_now_add=False, default=timezone.now)
     date_close_awards = models.DateTimeField("Finalizar Inscrições com Premiação", auto_now=False, auto_now_add=False, default=timezone.now)
     workload = models.DecimalField('Carga horária', max_digits=4, decimal_places=2, default=1, help_text='Em horas')
     is_closed = models.BooleanField('Está fechado?', default=False)
@@ -84,17 +92,24 @@ class Congress(models.Model):
     class Meta:
         verbose_name = 'Congresso'
         verbose_name_plural = 'Congressos'
+        ordering = ['name', '-date_start']
 
     def __str__(self):
         return self.name
 
     def get_date(self):
-        return "{}".format(self.date_start_congress.strftime("%m/%Y"))
+        return "{}".format(self.date_start.strftime("%m/%Y"))
     get_date.short_description = 'Data'
 
     def get_pdf_url(self):
         return format_html("<a href={}>pdf</a>".format(reverse('congress:report_list_pdf', kwargs={'slug': self.slug})))
     get_pdf_url.short_description = 'Lista Presenca'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify("{} {}".format(self.name, self.date_start.strftime("%Y")))
+
+        return super().save(*args, **kwargs)
 
 class Subscriptions(models.Model):
     """
@@ -111,48 +126,85 @@ class Subscriptions(models.Model):
         verbose_name_plural = 'Inscrições'
 
     def __str__(self):
-        return "{} ({})".format(self.user.name, self.congress.name)
+        return "{}".format(self.user.name)
 
-class Images(models.Model):
+    def get_congress_info(self):
+        return "{} ({})".format(self.congress.name, self.congress.date_start.strftime("%Y"))
+    get_congress_info.short_description = "Evento"
+
+class CertificateImages(models.Model):
     """
-        Imagem de Logo, Fundo de Cetificado e Logo de Instituicao 
+        Imagem de Fundo para certificados
     """
     congress = models.OneToOneField(Congress, on_delete=models.CASCADE)
-    logo = models.ImageField("Logo", default="default.png", upload_to=UploadToPathAndRename('congress/logo/'))
-    institute = models.ImageField("Instituto", default="default.png", upload_to=UploadToPathAndRename('congress/institute/'))
-    certificate = models.ImageField("Certificado", default="certificate.png", upload_to=UploadToPathAndRename('congress/certificates/'))
+    image = models.ImageField("Certificado", default="certificate.png", upload_to=UploadToPathAndRename('certificates/'))
 
     class Meta:
         verbose_name = 'Imagem'
         verbose_name_plural = 'Imagens'
 
     def __str__(self):
-        return "{} ({})".format(self.congress.username, self.congress.date_start_congress.strftime("%m/%Y"))
-
-    def get_detail_url(self):
-        return reverse("core:images_detail", kwargs={'pk': self.pk})
+        return "{} ({})".format(self.congress.username, self.congress.date_start.strftime("%m/%Y"))
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img_logo = Image.open(self.logo.path)
         img_cert = Image.open(self.certificate.path)
-
-        if img_logo.height > 300 or img_logo.width > 300:
-            output_size = (300,300)
-            img_logo.thumbnail(output_size)
-            img_logo.save(self.logo.path)
 
         if img_cert.height != 2480 or img_cert.width != 3508:
             output_size = (2480, 3508)
             img_cert.thumbnail(output_size)
             img_cert.save(self.certificate.path)
 
+        super().save(*args, **kwargs)
+
+class InstituteImages(models.Model):
+    """
+
+    """
+    congress = models.OneToOneField(Congress, on_delete=models.CASCADE)
+    image = models.ImageField("Instituto", default="default.png", upload_to=UploadToPathAndRename('institute/'))
+
+    class Meta:
+        verbose_name = 'Imagem'
+        verbose_name_plural = 'Imagens'
+
+    def __str__(self):
+        return "{} ({})".format(self.congress.username, self.congress.date_start.strftime("%m/%Y"))
+
+    def get_detail_url(self):
+        return reverse("core:images_detail", kwargs={'pk': self.pk})
+
+class LogoImages(models.Model):
+    """
+        Imagem de Logo, Fundo de Cetificado e Logo de Instituicao
+    """
+    congress = models.OneToOneField(Congress, on_delete=models.CASCADE)
+    image = models.ImageField("Logo", default="default.png", upload_to=UploadToPathAndRename('logo/'))
+
+
+    class Meta:
+        verbose_name = 'Imagem'
+        verbose_name_plural = 'Imagens'
+
+    def __str__(self):
+        return "{} ({})".format(self.congress.username, self.congress.date_start.strftime("%m/%Y"))
+
+    def get_detail_url(self):
+        return reverse("core:images_detail", kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        img_logo = Image.open(self.logo.path)
+
+        if img_logo.height > 300 or img_logo.width > 300:
+            output_size = (300,300)
+            img_logo.thumbnail(output_size)
+            img_logo.save(self.logo.path)
+
+        super().save(*args, **kwargs)
+
 class LuckyNumber(models.Model):
     """
         Numero para eventuais sorteios
     """
-
     congress = models.ForeignKey(Congress, verbose_name='Evento', on_delete=models.CASCADE)
     user = models.ForeignKey(Users, verbose_name="Usuario", on_delete=models.CASCADE)
     number = models.CharField("Numero da sorte", max_length=3)
