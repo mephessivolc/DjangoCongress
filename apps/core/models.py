2c498d4ba5bb1396935309a8849e2d63d208c2a3
Clovis Caface <clovis.caface@gmail.com>
2022-11-22 18:57:56 -0300

    create models in apps.core

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Congress(models.Model):
+
+    name = models.CharField("Nome", max_length=200)
+    short_name = models.CharField("Sigla", max_length=25)
+    first_day = models.DateTimeField("Início em")
+    last_day = models.DateTimeField("Término em")
+
+    class Meta:
+        verbose_name = "Evento"
+        verbose_name_plural = "Eventos"
+        ordering = ['short_name', 'name']
+
+    def __str__(self) -> str:
+        return f"{self.name}"
